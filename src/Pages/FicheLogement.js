5a06f9b9e16217e20785ad6513f5e224ad95817b
“AyoubTahar” <tahar.ayoub@gmail.com>
2023-03-03 19:30:48 +0100

    page erreur redirection si faux id

--- src/Pages/FicheLogement.js
+++ src/Pages/FicheLogement.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  useLocation, useParams } from 'react-router-dom';
+import {  useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Error from './Error';
@@ -8,14 +8,17 @@ import Information from '../components/Information';
 
 const FicheLogement = () => {
         const location = useLocation()
+        if (!location.state || !location.state.housedata) {
+            return <Error/>;
+        }
         const {housedata} = location.state
-    return (
-        <div>
-            <Header/>
-            <ImageSlider housedata={housedata} />
-            <Information tagdata={housedata} />
-            <Footer/>
-        </div>
+        return (
+            <div>
+                <Header/>
+                <ImageSlider housedata={housedata} />
+                <Information tagdata={housedata} />
+                <Footer/>
+            </div>
     );
 };
 
